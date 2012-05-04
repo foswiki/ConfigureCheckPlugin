e2c685d6ab44ffeb0d1b6ebfb06ef178475e6856
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/ConfigureCheckPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ConfigureCheckPlugin.pm
+++ lib/Foswiki/Plugins/ConfigureCheckPlugin.pm
@@ -9,7 +9,6 @@ cruftier bits of =configure=.
 
 =cut
 
-
 package Foswiki::Plugins::ConfigureCheckPlugin;
 
 use strict;
@@ -20,77 +19,83 @@ use Foswiki::Plugins ();    # For the API version
 
 our $VERSION = '$Rev: 13994 $';
 our $RELEASE = '1.0.0';
-our $SHORTDESCRIPTION = 'Run =configure= checkers from a plugin, to help debug a broken configuration';
+our $SHORTDESCRIPTION =
+'Run =configure= checkers from a plugin, to help debug a broken configuration';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
-    Foswiki::Func::registerTagHandler('CONFIGURE_CHECK', \&_CONFIGURE_CHECK)
-;#	if Foswiki::Func::isAnAdmin();
+    Foswiki::Func::registerTagHandler( 'CONFIGURE_CHECK', \&_CONFIGURE_CHECK )
+      ;                     #	if Foswiki::Func::isAnAdmin();
 
     return 1;
 }
 
 {
+
     # Keeps a count of errors and warnings seen so far
     package Counter;
 
     sub new {
-	my $class = shift;
-	return bless({}, $class);
+        my $class = shift;
+        return bless( {}, $class );
     }
 
     sub inc {
-	my ($this, $k) = @_;
-	$this->{$k}++;
+        my ( $this, $k ) = @_;
+        $this->{$k}++;
     }
 }
 
 sub _CONFIGURE_CHECK {
-    my ($session, $params) = @_;
+    my ( $session, $params ) = @_;
     $params->{format} ||= "| \$keys | \$answer |";
     require Foswiki::Configure::UI;
-    my $c = _doChecks('', new Counter(), $params );
-    return join($params->{separator} || "\n", @$c)
+    my $c = _doChecks( '', new Counter(), $params );
+    return join( $params->{separator} || "\n", @$c );
 }
 
 sub _doChecks {
-    my ($keys, $counter, $params) = @_;
+    my ( $keys, $counter, $params ) = @_;
     my $data = $keys ? eval("\$Foswiki::cfg$keys") : \%Foswiki::cfg;
     my @checked;
-    my $c = Foswiki::Configure::UI::loadChecker($keys, $counter);
+    my $c = Foswiki::Configure::UI::loadChecker( $keys, $counter );
     my $haveReport = 0;
     my $answer;
 
-    if (!$c) {
-	if (ref($data) eq 'HASH') {
-	    foreach my $key (sort keys %$data) {
-		if ($key =~ /^\w+$/) {
-		    my ($c, $i) = _doChecks("$keys\{$key\}", $counter, $params);
-		    push(@checked, @$c);
-		}
-	    }
-	    return \@checked;
-	}
-	$answer = "No checker";
-    } else {
-	$answer = $c->check();
-	if ($answer && $answer eq 'NOT USED IN THIS CONFIGURATION') {
-	    $answer = "Not used";
-	} elsif ($answer) {
-	    $answer =~ s/\n/ /g;
-	    $haveReport = 1;
-	} else {
-	    $answer = "OK";
-	}
+    if ( !$c ) {
+        if ( ref($data) eq 'HASH' ) {
+            foreach my $key ( sort keys %$data ) {
+                if ( $key =~ /^\w+$/ ) {
+                    my ( $c, $i ) =
+                      _doChecks( "$keys\{$key\}", $counter, $params );
+                    push( @checked, @$c );
+                }
+            }
+            return \@checked;
+        }
+        $answer = "No checker";
     }
-    if ($haveReport || Foswiki::Func::isTrue($params->{all})) {
-	my $fmt = $params->{format};
-	$fmt =~ s/\$keys/$keys/g;
-	$fmt =~ s/\$answer/$answer/g;
-	$fmt =~ Foswiki::Func::decodeFormatTokens( $fmt );
-	push(@checked, $fmt);
+    else {
+        $answer = $c->check();
+        if ( $answer && $answer eq 'NOT USED IN THIS CONFIGURATION' ) {
+            $answer = "Not used";
+        }
+        elsif ($answer) {
+            $answer =~ s/\n/ /g;
+            $haveReport = 1;
+        }
+        else {
+            $answer = "OK";
+        }
+    }
+    if ( $haveReport || Foswiki::Func::isTrue( $params->{all} ) ) {
+        my $fmt = $params->{format};
+        $fmt =~ s/\$keys/$keys/g;
+        $fmt =~ s/\$answer/$answer/g;
+        $fmt =~ Foswiki::Func::decodeFormatTokens($fmt);
+        push( @checked, $fmt );
     }
 
     return \@checked;
